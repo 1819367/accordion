4abf26779a60b9cd21c13ef7d375d5a85dd7e0af
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2025-07-25 16:14:01 -0700

    Animate (accordion) with javascript

--- src/main.js
+++ src/main.js
@@ -5,8 +5,19 @@ const accordionContainer = document.querySelector('.accordion__container')
 //add an event listener with event delegation
 accordionContainer.addEventListener('click', e => {
     const accordionHeader = e.target.closest('.accordion__header')
+
     if (accordionHeader) {
         const accordion = accordionHeader.parentElement
+        const accordionContent = accordionHeader.nextElementSibling
+        const accordionInner = accordionContent.children[0]
+
+        const height = accordionInner.getBoundingClientRect().height //get the height of accordionInner
+
+        if (accordion.classList.contains('is-open')) {
+            accordionContent.style.height = 0
+        } else {
+            accordionContent.style.height = height + 'px'  //set the correct height
+        }
         accordion.classList.toggle('is-open')
     } 
 })
